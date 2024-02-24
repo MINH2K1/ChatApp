17cce819d027d1f06bf851a695b4a4b36c8b6777
tranducminh <minhk19caothang@gmail.com>
2024-02-24 14:03:39 +0700

    create-interface-user

--- ChatApp.Application/Interface/IUserServicecs.cs
+++ ChatApp.Application/Interface/IUserServicecs.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatApp.Application.ViewModel;
+using ChatApp.Data.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace ChatApp.Application.Interface
 {
-    public class IUserServicecs
+    public interface IUserServicecs
     {
+        Task<bool> AddAsync(AppUserViewModel userVm);
+
+        Task DeleteAsync(string id);
+
+        Task<List<AppUserViewModel>> GetAllAsync();
+
+        PageResult<AppUserViewModel> GetAllPagingAsync(string keyword, int page, int pageSize);
+
+        Task<AppUserViewModel> GetById(string id);
+
+
+        Task UpdateAsync(AppUserViewModel userVm);
     }
 }
