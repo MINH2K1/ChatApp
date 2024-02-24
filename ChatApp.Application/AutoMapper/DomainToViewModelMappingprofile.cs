b15e336ad3d3ace807bd6bb87f46dd7eecf35a69
tranducminh <minhk19caothang@gmail.com>
2024-02-24 13:45:21 +0700

    create-iresponsitory

---
+++ ChatApp.Application/AutoMapper/DomainToViewModelMappingprofile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ChatApp.Application.ViewModel;
+using ChatApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Application.AutoMapper
+{
+   public class DomainToViewModelMappingprofile:Profile
+    {
+        public DomainToViewModelMappingprofile() {
+
+            CreateMap<AppUser, AppUserViewModel>();
+        }
+    }
+}
